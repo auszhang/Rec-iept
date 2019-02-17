4db071a2e83f444c29a4f5b6e587ca8f2486a270
Armaan Kohli <armaankohli@berkeley.edu>
2019-02-16 18:29:22 -0800

    ocr kinda works

--- ocr.py
+++ ocr.py
@@ -1,5 +1,8 @@
 import io
 import os
+import re
+# if re.match("^\d+?\.\d+?$", element) is None:
+#     print "Not float"
 
 class word():
     """docstring for word"""
@@ -25,29 +28,87 @@ def detect_text(path):
     print('Texts:')
 
     word_list = []
+    i = 0
 
     for text in texts:
         # print('\n"{}"'.format(text.description))
+        if i == 0:
+            i += 1
+        else:
+            vertices = []
+            for vertex in text.bounding_poly.vertices:
+                v = (vertex.x, vertex.y)
+                vertices.append(v)
+            # vertices = (['({},{})'.format(vertex.x, vertex.y)
+            #             for vertex in text.bounding_poly.vertices])
 
-        vertices = (['({},{})'.format(vertex.x, vertex.y)
-                    for vertex in text.bounding_poly.vertices])
+            # string = 'bounds: {}'.format(','.join(vertices))
+            # print(string)
 
-        # string = 'bounds: {}'.format(','.join(vertices))
-        # print(string)
+            new_word = word(text.description, vertices)
+            word_list.append(new_word)
+    return word_list
 
-        new_word = word(text.description, vertices)
-        word_list.append(new_word)
-
-def check_close(y_coord, dict_coords):
-    for coord in dict_coords:
-        if 
+def check_close(curr_y_coord, y_coords_keys):
+    # if len(y_coords_keys) == 0:
+    #     return curr_y_coord
+    for coord in y_coords_keys:
+        # print('Coord: ', coord)
+        # print('Current Coord: ', curr_y_coord)
+        if abs(coord - curr_y_coord) <= 5:
+            return coord
+        # else:
+        #     return 0
+    return 0
 
 def find_same_line(word_list):
     which_line = {}
     for w in word_list:
+        # print(w.description)
+        xD = check_close(w.lleft[1], which_line.keys())
+        # print(w.lleft[1])
+        if xD:
+            # print(xD)
+            # print('fffffff')
+            which_line[xD] = which_line[xD] + " " + w.description
+        else:
+            # print(int(w.lleft[1]))
+            # print('truuu')
+            which_line[w.lleft[1]] = w.description
+    return which_line
 
-        if w.lleft[1] not in w
+def extract_item_price(line_dic):
+    new_dic = {}
+    quantity_dic = {}
+    for k,v in line_dic.items():
+        split_value = v.split(" ")
+        last_item = split_value[len(split_value) - 1]
+        last_item = re.sub('[^0-9\.]', '', last_item)
+        if re.match("^\d+\.\d+$", last_item):
+            first_item = split_value[0]
+            if re.match("^\d+$", first_item):
+                new_key = " ".join(split_value[:-1])
+                new_key = new_key.lstrip('0123456789.- ')
+                quantity_dic[new_key] = int(first_item)
+                new_dic[new_key] = float(last_item)
+            else:
+                new_key = " ".join(split_value[:-1])
+                quantity_dic[new_key] = 1
+                new_dic[new_key] = float(last_item)
+    return new_dic
 
 
-detect_text('./inputs/electronic/electronic1.jpg')
+        # last_item = split_value[len(split_value) - 1]
+        # last_item = re.sub('[^0-9\.]', '', last_item)
+        #     if re.match("^\d+$", first_item):
+        #         new_key = " ".join(split_value[:-1])
+        #         quantity_dic[new_key] = int(first_item)
+        #         new_key.lstrip('0123456789.- ')
+            
+        #     new_dic[new_key] = float(last_item)
 
+w_list = detect_text('./inputs/easy/easy1.jpg')
+tester = find_same_line(w_list)
+print(tester)
+yo = extract_item_price(tester)
+print(yo)
