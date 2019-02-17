6c082c637132bc7a4fdcd96b4e6e7e3c888ebe6e
Armaan Kohli <armaankohli@berkeley.edu>
2019-02-16 16:25:58 -0800

    adding key to repo

--- ocr.py
+++ ocr.py
@@ -1,3 +1,15 @@
+import io
+import os
+
+class word():
+    """docstring for word"""
+    def __init__(self, description, vertices):# lower_left, lower_right, upper_right, upper_left):
+        self.description = description
+        self.lleft = vertices[0]
+        self.lright = vertices[1]
+        self.uright = vertices[2]
+        self.uleft = vertices[3]
+
 def detect_text(path):
     """Detects text in the file."""
     from google.cloud import vision
@@ -12,14 +24,30 @@ def detect_text(path):
     texts = response.text_annotations
     print('Texts:')
 
+    word_list = []
+
     for text in texts:
-        print('\n"{}"'.format(text.description))
+        # print('\n"{}"'.format(text.description))
 
         vertices = (['({},{})'.format(vertex.x, vertex.y)
                     for vertex in text.bounding_poly.vertices])
 
-        print('bounds: {}'.format(','.join(vertices)))
+        # string = 'bounds: {}'.format(','.join(vertices))
+        # print(string)
+
+        new_word = word(text.description, vertices)
+        word_list.append(new_word)
+
+def check_close(y_coord, dict_coords):
+    for coord in dict_coords:
+        if 
+
+def find_same_line(word_list):
+    which_line = {}
+    for w in word_list:
+
+        if w.lleft[1] not in w
 
 
-if __name__ == 'main':
-    detect_text('./inputs/electronic/electronic1.jpg')
+detect_text('./inputs/electronic/electronic1.jpg')
+
