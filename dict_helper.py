e69ac7df7e343b4aedf936dcfa72a4810d24667d
Armaan Kohli <armaankohli@berkeley.edu>
2019-02-17 06:35:47 -0800

    l8 upd8

--- dict_helper.py
+++
@@ -1,9 +0,0 @@
-import json
-from main import item_price as ip
-
-print ip
-
-# SAMPLE INFORMATION FOR TESTING: obtain actual values from GCP:
-# sample_easy1 = {'BJS FRITOS NACHOS':7.50, 'BH DBL DELUXE BURGER':10.95}
-# print json.dumps(sample_easy1)
-
--- main.py
+++ main.py
@@ -14,17 +14,31 @@ configure_uploads(app, photos)
 
 filename = ""
 item_price = {}
+namelist = []
+item_name = {}
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST' and 'photo' in request.files:
     	filename = photos.save(request.files['photo'])
     	pather = 'static/img/' + str(filename)
+    	global item_price 
     	item_price = ocr.mainer(pather)
-    	json.dumps(item_price)
-    	return render_template('breakdown.html', dict1 = {"name":1,"assd":2})
+    	size = len(item_price)
+    	return render_template('breakdown.html', dict1 = item_price)
     return render_template('upload.html')
 
+@app.route('/breakdown', methods=['GET', 'POST'])
+def breakdown():
+	if request.method == 'POST':
+		# print(str(item_price))
+		i = 0
+		for k,v in item_price.items():
+			# print(request.form[])
+			item_name[k] = request.form[k]
+			# i += 1 
+		return render_template('end.html', dict2 = item_name)
+	return render_template('breakdown.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
