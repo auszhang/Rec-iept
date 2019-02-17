3e6b325a16e8db8c46e6b98e9fc7e14c46ed4ef6
Austin <austinchiang1@berkeley.edu>
2019-02-17 00:35:56 -0800

    flask is working explosion bitches

--- main.py
+++ main.py
@@ -13,18 +13,12 @@ configure_uploads(app, photos)
 
 
 @app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
+def upload():
     if request.method == 'POST' and 'photo' in request.files:
     	filename = photos.save(request.files['photo'])
     	return filename
     return render_template('upload.html')
-    #     file = request.files['image']
-    #     f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        
-    #     # add your custom code to check that the uploaded file is a valid image and not a malicious file (out-of-scope for this post)
-    #     file.save(f)
 
-    # return render_template('end.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
