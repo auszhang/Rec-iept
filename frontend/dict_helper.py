932e54e9ee3e572911d63f7a46139dbc14a2e66e
Austin Zhang <aus.zhang@gmail.com>
2019-02-17 00:53:02 -0800

    finished breakdown page

--- frontend/dict_helper.py
+++
@@ -1,5 +0,0 @@
-import json
-
-# SAMPLE INFORMATION FOR TESTING: obtain actual values from GCP:
-sample_easy1 = {'BJS FRITOS NACHOS':7.50, 'BH DBL DELUXE BURGER':10.95}
-print json.dumps(sample_easy1)
